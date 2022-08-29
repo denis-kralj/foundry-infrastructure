ecb0247237e0bbcfc24041786ffb0c45d5431e11
Denis Kralj <denis-kralj@users.noreply.github.com>
2022-08-29 20:30:46 +0200

    Change inventory format to yaml Update gitignore Add ssh retry count to counter lazy aws

--- terraform/generated.tf
+++ terraform/generated.tf
@@ -1,4 +1,13 @@
-resource "local_file" "foo" {
-    content  = "foundry-server ansible_host=${module.foundry.instance_ip} ansible_connection=ssh ansible_user=ubuntu ansible_host_key_checking=False"
-    filename = "../ansible/inventory.ini"
+resource "local_file" "ansible_inventory" {
+    content  = <<EOT
+all:
+  hosts:
+    foundry-server:
+      ansible_host: ${module.foundry.instance_ip}
+      ansible_connection: ssh
+      ansible_user: ubuntu
+      ansible_host_key_checking: False
+      ansible_ssh_retries: 10
+EOT
+    filename = "../ansible/inventory.yaml"
 }
